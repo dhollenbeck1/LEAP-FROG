a7a2fa0e435513e9e74c7bc6ddd475a72a335c8d
Derek Hollenbeck <30532879+dhollenbeck1@users.noreply.github.com>
2022-07-28 19:23:16 -0700

    demo waypoints

--- statemachine-sitl/VehicleClass3.py
+++ statemachine-sitl/VehicleClass3.py
@@ -522,6 +522,57 @@ class UAV:
         print('num of waypoints: %s' % num_waypoints)
         return num_waypoints
     
+    def upload_mission(self,aFileName):
+        """
+        Upload a mission from a file. 
+        """
+        #Read mission from file
+        missionlist = self.readmission(aFileName)
+    
+        print("\nUpload mission from a file: %s" % aFileName)
+        #Clear existing mission from vehicle
+        print(' Clear mission')
+        cmds = self.vehicle.commands
+        cmds.clear()
+        #Add new mission to vehicle
+        for command in missionlist:
+            cmds.add(command)
+        print(' Upload mission')
+        self.vehicle.commands.upload()
+    
+    def readmission(aFileName):
+        """
+    Load a mission from a file into a list. The mission definition is in the Waypoint file
+    format (http://qgroundcontrol.org/mavlink/waypoint_protocol#waypoint_file_format).
+
+    This function is used by upload_mission().
+    """
+        print("\nReading mission from file: %s" % aFileName)
+        cmds = self.vehicle.commands
+        missionlist=[]
+        with open(aFileName) as f:
+            for i, line in enumerate(f):
+                if i==0:
+                    if not line.startswith('QGC WPL 110'):
+                        raise Exception('File is not supported WP version')
+                else:
+                    linearray=line.split('\t')
+                    ln_index=int(linearray[0])
+                    ln_currentwp=int(linearray[1])
+                    ln_frame=int(linearray[2])
+                    ln_command=int(linearray[3])
+                    ln_param1=float(linearray[4])
+                    ln_param2=float(linearray[5])
+                    ln_param3=float(linearray[6])
+                    ln_param4=float(linearray[7])
+                    ln_param5=float(linearray[8])
+                    ln_param6=float(linearray[9])
+                    ln_param7=float(linearray[10])
+                    ln_autocontinue=int(linearray[11].strip())
+                    cmd = Command( 0, 0, 0, ln_frame, ln_command, ln_currentwp, ln_autocontinue, ln_param1, ln_param2, ln_param3, ln_param4, ln_param5, ln_param6, ln_param7)
+                    missionlist.append(cmd)
+        return missionlist
+    
     def get_distance_metres(self, aLocation1, aLocation2):
         """
         Returns the ground distance in metres between two LocationGlobal objects.
